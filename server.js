dd4cfa8faa2890c7a9e97573cb0ad799d74d5f29
ericstober <erictstober@gmail.com>
2025-05-27 11:50:16 -0500

    clean up use return inside if and remove the else

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.get("/api/posts", (req, res) => {
   const limit = parseInt(req.query.limit);
 
   if (!isNaN(limit) && limit > 0) {
-    res.status(200).json(posts.slice(0, limit));
-  } else {
-    res.status(200).json(posts);
+    return res.status(200).json(posts.slice(0, limit));
   }
+
+  res.status(200).json(posts);
 });
 
 // Get single post
@@ -30,10 +30,10 @@ app.get("/api/posts/:id", (req, res) => {
   const post = posts.find((post) => post.id === id);
 
   if (!post) {
-    res.status(404).json({ message: `A post with the id of ${id} was not found` });
-  } else {
-    res.status(200).json(post);
+    return res.status(404).json({ message: `A post with the id of ${id} was not found` });
   }
+
+  res.status(200).json(post);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
