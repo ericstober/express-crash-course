624265b76342e4dc957aace50445314408b68037
ericstober <erictstober@gmail.com>
2025-05-27 13:30:45 -0500

    add create, update, and delete routes to api

--- routes/posts.js
+++ routes/posts.js
@@ -31,4 +31,45 @@ router.get("/:id", (req, res) => {
   res.status(200).json(post);
 });
 
+// Create new post
+router.post("/", (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+  };
+
+  if (!newPost.title) {
+    return res.status(400).json({ message: "Please include a title" });
+  }
+
+  posts.push(newPost);
+  res.status(201).json(posts);
+});
+
+// Update Post
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({ message: `A post with the id of ${id} was not found` });
+  }
+
+  post.title = req.body.title;
+  res.status(200).json(posts);
+});
+
+// Delete Post
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({ message: `A post with the id of ${id} was not found` });
+  }
+
+  posts = posts.filter((post) => post.id !== id);
+  res.status(200).json(posts);
+});
+
 export default router;
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Setup static folder
 // app.use(express.static(path.join(__dirname, "public")));
 
