bf8dc67a9a2b2d7f3b94453cd9404c1ccbcb2a47
ericstober <erictstober@gmail.com>
2025-05-27 11:05:04 -0500

    static server example

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Setup static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(5000, () => console.log(`Server is running on port 5000`));
