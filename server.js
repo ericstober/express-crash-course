ee064f00f9da14300a99edb1b80cf77fe3f46517
ericstober <erictstober@gmail.com>
2025-05-27 11:54:42 -0500

    move posts routes to own file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const posts = require("./routes/posts");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -7,33 +8,7 @@ const app = express();
 // Setup static folder
 // app.use(express.static(path.join(__dirname, "public")));
 
-let posts = [
-  { id: 1, title: "Post One" },
-  { id: 2, title: "Post Two" },
-  { id: 3, title: "Post Three" },
-];
-
-// Get all posts
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-
-  res.status(200).json(posts);
-});
-
-// Get single post
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-
-  if (!post) {
-    return res.status(404).json({ message: `A post with the id of ${id} was not found` });
-  }
-
-  res.status(200).json(post);
-});
+// Routes
+app.use("/api/posts", posts);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
